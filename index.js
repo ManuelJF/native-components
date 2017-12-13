c574a1a6a48a5a91d9f9ed6366ea5ab2eb5dabaf
Luis Juarez <skaflon_war@hotmail.com>
2017-12-12 20:35:24 -0600

    first intent

---
+++ index.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components/native'
+
+export const Button = styled.TouchableOpacity`
+  backgroundColor: #FF5722;
+`
+
+export const Paragraph = styled.Text`
+  font-size: 16px;
+  color: #FFF;
+`
